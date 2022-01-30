8b49eaf5ef4423acccaa7145e7cc1f8d04729f61
Wendy Osorio <98280294+Wendy-Osorio@users.noreply.github.com>
2022-01-29 23:15:23 -0600

    Subir archivos

---
+++ ApiPeliculas.Domain/Entities/Director.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace ApiPeliculas.Domain.Entities
+{
+    public partial class Director
+    {
+        public int IdDirector { get; set; }
+        public string Nombre { get; set; }
+        public int IdPelicula { get; set; }
+
+        public virtual Pelicula Pelicula{ get; set; }
+    }
+}
